541eba727f10a106c85fd1f30dab36742ba48a51
lauralimachi <erickalaura133@gmail.com>
2024-07-22 21:36:12 -0400

    Creacion de base de datos

--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+# class Proveedores(models.Model):
+#     nombre_prov = models.CharField(max_length=255) not null
+#     direccion_prov = models.CharField(max_length=255)
+#     telefono_prov = models.CharField(max_length=255)
+
+
+#     def __str__(self):
+#         return f'Proveedor {self.id}: {self.nombre_prov} {self.telefono_prov}'
+
+class Producto(models.Model):
+    nombre_pro = models.CharField(max_length=255)
+    descripcion_pro = models.CharField(max_length=255)
+    precio_pro = models.DecimalField(max_digits=10, decimal_places=2)
+    cantidad_pro = models.IntegerField()
+    # proveedor_id_pro = models.ForeignKey(Proveedores, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f'Producto {self.id}: {self.nombre_pro} {self.precio_pro}'
+
+class Inventario(models.Model):
+    cantidad_inv = models.IntegerField()
+    producto_id_inv = models.ForeignKey(Producto, on_delete=models.SET_NULL, null=True)
+
+
+class Cliente(models.Model):
+    nombre_cli = models.CharField(max_length=255)
+    documento_cli = models.CharField(max_length=255)
+
+class Venta(models.Model):
+    fecha_vta = models.DateField()
+    total_vta = models.DecimalField(max_digits=10, decimal_places=2)
+    cliente_id = models.ForeignKey(Cliente,on_delete=models.SET_NULL, null=True)
+
+
+class DetalleVenta(models.Model):
+    cantidad_dv = models.IntegerField()
+    precio_unitario_dv = models.DecimalField(max_digits=10, decimal_places=2) 
+    total_linea = models.DecimalField(max_digits=10, decimal_places=2) 
+    producto_id_dv = models.ForeignKey(Producto,on_delete=models.SET_NULL, null=True)
+    venta_id_dv = models.ForeignKey(Venta,on_delete=models.SET_NULL, null=True)
+    
+    
